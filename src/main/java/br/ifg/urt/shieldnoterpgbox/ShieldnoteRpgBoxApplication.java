b058b8aa5cd1ca5264e10e2a093fe17b0fd55775
Vinícius <vinicipc@gmail.com>
2026-05-07 14:35:11 -0300

    Commit inicial do projeto WEBII

---
+++ src/main/java/br/ifg/urt/shieldnoterpgbox/ShieldnoteRpgBoxApplication.java
@@ -0,0 +1,13 @@
+package br.ifg.urt.shieldnoterpgbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShieldnoteRpgBoxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShieldnoteRpgBoxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/ifg/urt/shieldnoterpgbox/ShieldnoteRpgBoxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.ifg.urt.shieldnoterpgbox;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShieldnoteRpgBoxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
